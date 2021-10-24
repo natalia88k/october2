1e597e2800b53e714394dfed7f49eb317a43b9a5
natalia88k <natalia88k@yandex.ru>
2021-10-24 17:55:18 +0300

    Remove

--- MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package October2;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(int rowIndex, int columnIndex, String value) {
-        super("Value on row # " + rowIndex + ", column # " + columnIndex + " is not a valid integer: " + value);
-    }
-}
